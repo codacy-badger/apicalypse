be221fbe5c4259fa3ad92886b7a7f829322ecc21
Markus Winter <markus@wntr.me>
2019-01-23 00:27:09 +0100

    Fix linter issues

--- Tests/ApicalypseTests/ApicalypseTests.swift
+++ Tests/ApicalypseTests/ApicalypseTests.swift
@@ -10,6 +10,6 @@ final class ApicalypseTests: XCTestCase {
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testExample", testExample)
     ]
 }
--- Tests/ApicalypseTests/XCTestManifests.swift
+++ Tests/ApicalypseTests/XCTestManifests.swift
@@ -3,7 +3,7 @@ import XCTest
 #if !os(macOS)
 public func allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(ApicalypseTests.allTests),
+        testCase(ApicalypseTests.allTests)
     ]
 }
 #endif
